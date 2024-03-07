f8f51cd5840ffde2c90027d80e18890d20dd70ce
garamflow <garamflow@gmail.com>
2024-03-07 21:23:22 +0900

    [refactor] lombok을 이용한 리팩터링

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class MemberController {
 
     @Autowired
@@ -21,11 +23,11 @@ public class MemberController {
 
     @PostMapping("/join")
     public String join(MemberForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
         Member member = form.toEntity();
-        System.out.println(member.toString());
+        log.info(member.toString());
         Member saved = memberRepository.save(member);
-        System.out.println(saved);
+        log.info(saved.toString());
         return "";
     }
 }
