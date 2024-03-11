2520885de55d976b9d0106509b3a30123ac362da
garamflow <garamflow@gmail.com>
2024-03-12 00:18:59 +0900

    [feat] 멤버 수정하기 기능 구현

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO article(id, title, content) VALUES (1, '가가가', '111');
 INSERT INTO article(id, title, content) VALUES (2, '나나나', '222');
 INSERT INTO article(id, title, content) VALUES (3, '다다다', '333');
+
+INSERT INTO member(id, email, password) VALUES (1, '111@aaa', 'asdf');
+INSERT INTO member(id, email, password) VALUES (2, '222@bbb', 'asdf');
+INSERT INTO member(id, email, password) VALUES (3, '333@ccc', 'asdf');
