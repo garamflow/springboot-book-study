5de622b5c3a20fa19130deaf4541b1b884ffc213
garamflow <garamflow@gmail.com>
2024-03-18 21:06:28 +0900

    [feat] coffee 테이블 생성 및 초기 데이터 집어넣기

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,7 @@ INSERT INTO article(title, content) VALUES ('다다다', '333');
 INSERT INTO member(id, email, password) VALUES (1, '111@aaa', 'asdf');
 INSERT INTO member(id, email, password) VALUES (2, '222@bbb', 'asdf');
 INSERT INTO member(id, email, password) VALUES (3, '333@ccc', 'asdf');
+
+INSERT INTO coffee(name, price) VALUES ('아메리카노', '4500');
+INSERT INTO coffee(name, price) VALUES ('라떼', '5000');
+INSERT INTO coffee(name, price) VALUES ('카페 모카', '5500');
