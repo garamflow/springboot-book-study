43e652f51265da78aeae5186c8cfa4ef4065932f
garamflow <garamflow@gmail.com>
2024-03-18 22:46:10 +0900

    [feat] 트랜잭션 예시 코드 추가

--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -4,13 +4,14 @@ import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class ArticleService {
 
     @Autowired
@@ -50,4 +51,16 @@ public class ArticleService {
         return target;
     }
 
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+        List<Article> articleList = dtos.stream()
+                .map(dto -> dto.toEntity())
+                .collect(Collectors.toList());
+
+        articleList.stream().forEach(article -> articleRepository.save(article));
+
+        articleRepository.findById(-1L)
+                .orElseThrow(() -> new IllegalArgumentException("결제 실패"));
+
+        return articleList;
+    }
 }
