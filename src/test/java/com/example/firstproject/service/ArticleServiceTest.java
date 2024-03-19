e744ea1913a59e2f9ec007c373886bb118add4a2
garamflow <garamflow@gmail.com>
2024-03-19 22:32:29 +0900

    [feat] JUnit5를 이용한 테스트 케이스 작성 구현

---
+++ src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -0,0 +1,81 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class ArticleServiceTest {
+
+    @Autowired
+    ArticleService articleService;
+
+    @Test
+    void index() {
+        Article a = new Article(1L, "가가가", "111");
+        Article b = new Article(1L, "나나나", "222");
+        Article c = new Article(1L, "다다다", "333");
+        List<Article> expected = new ArrayList<Article>(Arrays.asList(a, b, c));
+
+        List<Article> articleList = articleService.index();
+
+        assertEquals(expected.toString(), articleList.toString());
+    }
+
+    @Test
+    void show_성공_존재하는_id_입력() {
+        Long id = 1L;
+
+        Article expected = new Article(1L, "가가가", "111");
+
+        Article article = articleService.show(id);
+
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    void show_실패_존재하지_않는_id_입력() {
+        Long id = 1L;
+
+        Article expected = null;
+
+        Article article = articleService.show(id);
+
+        assertEquals(expected, article);
+    }
+
+    @Test
+    void create_성공_title과_content만_있는_dto_입력() {
+        String title = "라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(null, title, content);
+        Article expected = new Article(4L, title, content);
+
+        Article article = articleService.create(dto);
+
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    void create_실패_id가_포함된_dto_입력() {
+        Long id = 4L;
+        String title = "라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(id, title, content);
+        Article expected = null;
+
+        Article article = articleService.create(dto);
+
+        assertEquals(expected, article);
+    }
+}
