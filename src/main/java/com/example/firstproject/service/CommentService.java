70342d95015bf72b065fcbc58f91f90d8ce55b9f
garamflow <garamflow@gmail.com>
2024-03-22 00:32:10 +0900

    [feat] 댓글 컨트롤러와 서비스 구현

---
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -0,0 +1,27 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId) {
+        return commentRepository.findByArticleId(articleId)
+                .stream()
+                .map(comment -> CommentDto.createCommentDto(comment))
+                .collect(Collectors.toList());
+    }
+}
