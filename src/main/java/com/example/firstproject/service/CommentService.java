a1e886804e553559e607a68f4cf2cd12a3222b9c
garamflow <garamflow@gmail.com>
2024-03-25 22:32:43 +0900

    [feat] 댓글 등록 컨트롤러, 서비스, DTO 메소드 구현

--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -1,10 +1,13 @@
 package com.example.firstproject.service;
 
 import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.entity.Comment;
 import com.example.firstproject.repository.ArticleRepository;
 import com.example.firstproject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,5 +26,17 @@ public class CommentService {
                 .stream()
                 .map(comment -> CommentDto.createCommentDto(comment))
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CommentDto create(Long articleId, CommentDto dto) {
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패" +
+                        "대상 게시글이 없습니다."));
+
+        Comment comment = Comment.createComment(dto, article);
+        Comment created = commentRepository.save(comment);
+
+        return CommentDto.createCommentDto(created);
     }
 }
